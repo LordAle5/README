17669a5c4c31044034df0412086e180e273d128f
LordAle5 <121577581+LordAle5@users.noreply.github.com>
2022-12-28 14:34:14 -0500

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,38 @@
+package com.proyect;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int a = 23;
+            System.out.println(a);
+
+        String xd = "Hi world";
+            System.out.println(xd);
+
+        double v1 = 32.23;
+            System.out.println(v1);
+
+        long v2 = 3214123;
+            System.out.println(v2);
+
+        boolean v3 = false;
+            System.out.println(v3);
+            
+        boolean v4 = true;
+            System.out.println(v4);
+
+        float v5 = 2.3f;
+            System.out.println(v5);
+
+        System.out.println("Fin");
+        
+    }
+
+
+
+
+
+
+    
+}
